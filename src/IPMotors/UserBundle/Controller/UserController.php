ecec09669b636289a63738271cc2bd300af86a1f
tdubuffet <dubuffet.thibault@gmail.com>
2014-05-20 12:45:11 +0200

    Administration des roles FOSUser

--- src/IPMotors/UserBundle/Controller/UserController.php
+++ src/IPMotors/UserBundle/Controller/UserController.php
@@ -3,8 +3,33 @@
 namespace IPMotors\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use IPMotors\UserBundle\Entity\User;
 
 class UserController extends Controller {
+
+    public function usersListAction() {
+        //access user manager services 
+
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+
+
+        return $this->render('IPMotorsUserBundle::list.html.twig', array('users' => $users));
+    }
+
+    public function promoteAdminAction($username) {
+
+        $usermanip = $this->get('fos_user.util.user_manipulator');
+        $usermanip->addRole($username, 'ROLE_ADMIN');
+
+        return $this->redirect($this->generateUrl('ip_motors_users_list'));
+    }
+
+    public function demoteAdminAction($username) {
+
+        $usermanip = $this->get('fos_user.util.user_manipulator');
+        $usermanip->removeRole($username, 'ROLE_ADMIN');
+
+        return $this->redirect($this->generateUrl('ip_motors_users_list'));
+    }
 
 }
