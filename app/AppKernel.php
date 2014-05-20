ce039522851d6c79ad782f3db6463255c56c9724
KeurbyCandy <cavannachristophe@gmail.com>
2014-05-20 12:26:37 +0200

    Homepage + Read/Update/Delete Customer Module

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new IPMotors\UserBundle\IPMotorsUserBundle(),
             new IPMotors\WebServiceBundle\IPMotorsWebServiceBundle(),
             new IPMotors\CustomerBundle\IPMotorsCustomerBundle(),
+            new IPMotors\FormBundle\IPMotorsFormBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
